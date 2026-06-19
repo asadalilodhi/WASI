47cefb2cdb32bd08ee46d61f425414d07cc4c781
asadalilodhi <asadalilodhi09@gmail.com>
2026-06-20 00:47:52 +0500

    Hardcode supabase keys for vercel deployment

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://jeettuybmkqsxoyjwvxo.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImplZXR0dXlibWtxc3hveWp3dnhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4MDY4MTIsImV4cCI6MjA5NzM4MjgxMn0.tUK8f2y54hC4tJoQnGWXrNpRH_4ZpMvAe7D0tPrX0sM';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
